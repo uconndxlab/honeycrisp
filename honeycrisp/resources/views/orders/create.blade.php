b51e44fcbd7e9c875a39ed4b6c3462add2723d77
Joel Salisbury <joel.salisbury@gmail.com>
2024-04-24 15:55:16 -0400

    frontend stuff for items

--- honeycrisp/resources/views/orders/create.blade.php
+++ honeycrisp/resources/views/orders/create.blade.php
@@ -3,113 +3,215 @@
 @section('title', 'Create Order')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div clas="col-md-12">
-                <h2>Create Order ({{ $facility->abbreviation }}) </h2>
-                <!-- card for which facility the order is for -->
-                <div class="card my-2">
-                    <div class="card-body">
-                        <p class="card-text">Facility Name: {{ $facility->name }}</p>
-                        <p class="card-text">Facility Email: {{ $facility->email }}</p>
-                    </div>
+    <form action="{{ route('orders.store') }}" method="POST">
+        @csrf
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Create Order</h1>
                 </div>
             </div>
-        </div>
-        <div class="row">
 
-            <form action="{{ route('orders.store') }}" method="POST">
-                @csrf
+            <div class="row">
 
-                <div class="col-md-12">
-                    <h3>Order Details</h3>
-
-                    <!-- Customer Information -->
-                    <div class="accordion my-2" id="customerInformationAccordion">
+                <div class="col-md-6">
+                    <div class="accordion my-2" id="facilityInformationAccordion">
                         <div class="accordion-item">
-                            <h2 class="accordion-header" id="customerInformationHeading">
+                            <h2 class="accordion-header" id="facilityInformationHeading">
                                 <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#customerInformationCollapse" aria-expanded="true"
-                                    aria-controls="customerInformationCollapse">
-                                    Customer Information
+                                    data-bs-target="#facilityInformationCollapse" aria-expanded="true"
+                                    aria-controls="facilityInformationCollapse">
+                                    Facility Information
                                 </button>
                             </h2>
-                            <div id="customerInformationCollapse" class="accordion-collapse collapse show"
-                                aria-labelledby="customerInformationHeading" data-bs-parent="#customerInformationAccordion">
+                            <div id="facilityInformationCollapse" class="accordion-collapse collapse show"
+                                aria-labelledby="facilityInformationHeading" data-bs-parent="#facilityInformationAccordion">
                                 <div class="accordion-body">
-                                    <!-- User ID -->
-                                    <div class="form-group my-2">
-                                        <label for="user_id">Ordering for User:</label>
-                                        <input type="text" name="user_id" id="user_id" class="form-control"
-                                            value="{{ Auth::id() }}">
-                                    </div>
+                                    <p class="card-text">
+                                        <a href="{{ route('facilities.show', $facility->id) }}">
+                                            {{ $facility->name }}
+                                            ({{ $facility->abbreviation }})
+                                        </a>
+                                    </p>
+                                    <p class="card-text"><a href="mailto:{{ $facility->email }}">{{ $facility->email }}</a>
+                                    <p class="card-text">{{ $facility->description }}</p>
 
-                                    <!-- Payment Account -->
-                                    <div class="form-group my-2">
-                                        <label for="payment_account">Payment Account:</label>
-                                        <input type="text" name="payment_account" id="payment_account"
-                                            class="form-control" value="">
-                                    </div>
-
-                                    <!-- Price Group -->
-
-                                    <div class="form-group my-2">
-                                        <label for="price_group">Price Group:</label>
-                                        <div class="form-check form-switch">
-                                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
-                                            <label class="form-check-label" for="flexSwitchCheckChecked">Use External Rates</label>
-                                        </div>
-                                    </div>
-                                    
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="row my-3 py-3">
-                    <div class="col-md-8">
-                        <h3>Available Items</h3>
+            <div class="col-md-6">
+                <!-- Customer Information -->
+                <div class="accordion my-2" id="customerInformationAccordion">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="customerInformationHeading">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#customerInformationCollapse" aria-expanded="true"
+                                aria-controls="customerInformationCollapse">
+                                Customer Information
+                            </button>
+                        </h2>
+                        <div id="customerInformationCollapse" class="accordion-collapse collapse show"
+                            aria-labelledby="customerInformationHeading" data-bs-parent="#customerInformationAccordion">
+                            <div class="accordion-body">
+                                <!-- User ID -->
+                                <div class="form-group my-2">
+                                    <label for="user_id">Ordering for User:</label>
+                                    <input type="text" name="user_id" id="user_id" class="form-control"
+                                        value="jrs06005 (Salisbury, Joel R)" readonly>
+                                </div>
 
-                        <!-- little search box start typing and it filters the products -->
-                        <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Search for products" aria-label="Search"
-                                aria-describedby="button-addon2">
-                            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
-                        </div>
+                                <!-- Payment Account -->
+                                <div class="form-group my-2">
+                                    <label for="payment_account">Payment Account:</label>
+                                    <input type="text" name="payment_account" id="payment_account"
+                                        value="KFS-6215250-6610 (WellSCAN PHA 2021)" class="form-control" value="">
+                                </div>
 
-                        <!-- button for add custom product -->
-                        <div class="form-group my-2 text-end">
-                            <button type="button" class="btn btn-primary">Add Custom Item</button>
-                        </div>
+                                <!-- Price Group -->
 
-                        <!-- list of products in a ul -->
-                        <ul class="list-group">
-                            <li class="list-group-item"><a href="#" class="list-group-item-action">Student Web Development ($25 per hour)</a></li>
-                            <li class="list-group-item"><a href="#" class="list-group-item-action">Facility Director Time ($150 per hour)</a></li>
-                            <li class="list-group-item"><a href="#" class="list-group-item-action">Senior Application Developer ($150 per hour)</a></li>
-                            <li class="list-group-item"><a href="#" class="list-group-item-action">Graduate Student Times ($51 per hour)</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-4">
-                        <h3>Current Order</h3>
-                        
-                        <div class="alert alert-info">
-                            <p>Order Total: $0.00</p>
-                        </div>
+                                <div class="form-group my-2">
+                                    <label for="price_group">Price Group:</label>
+                                    <div class="form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" role="switch"
+                                            id="flexSwitchCheckChecked">
+                                        <label class="form-check-label" for="flexSwitchCheckChecked">Use External
+                                            Rates</label>
+                                    </div>
+                                </div>
 
-                        <div class="form-group my-2 text-end">
-                            <button type="submit" class="btn btn-primary">Submit Order</button>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
 
-              
+    <div class="container-fluid">
+        <div class="row bg-dark text-white">
+            <div class="row my-3 py-3">
+                <div class="col-md-8">
+                    <h3>Available Items</h3>
+
+                    <!-- little search box start typing and it filters the products -->
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Search for products" aria-label="Search"
+                            aria-describedby="button-addon2">
+                        <button class="btn btn-secondary text-white" type="button" id="button-addon2">Search</button>
+                    </div>
+
+                    <!-- button for add custom product -->
+                    <div class="form-group my-2 text-end">
+                        <button 
+                            data-bs-toggle="modal" 
+                            data-bs-target="#addCustomItemModal"
+                        type="button" class="btn btn-primary">
+                            Add Custom Item
+                        </button>
+                    </div>
+
+                    <!-- list of products in a ul -->
+                    <ul class="list-group">
+                        <li class="list-group-item"><a href="#" class="list-group-item-action text-accent">Student
+                                Web Development ($25 per hour)</a></li>
+                        <li class="list-group-item"><a href="#" class="list-group-item-action text-accent">Facility
+                                Director Time ($150 per hour)</a>
+                        </li>
+                        <li class="list-group-item"><a href="#" class="list-group-item-action text-accent">Senior
+                                Application Developer ($105 per hour)</a></li>
+                        <li class="list-group-item"><a href="#" class="list-group-item-action text-accent">Graduate
+                                Student Times ($51 per hour)</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h5>Current Order</h5>
+
+                    <div class="alert alert-info">
+                        <!-- Cart-like list of products with remove and quantity -->
+                        <ul class="list-group">
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                Student Web Development <br> $137.50
+                                <div class="input-group input-group-sm" style="width: 100px;">
+                                    <input type="number" class="form-control" value="5.5" min="1">
+                                    <button class="btn btn-danger btn-sm">X</button>
+                                </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                Facility Director Time <br> $300.00
+                                <div class="input-group input-group-sm" style="width: 100px;">
+                                    <input type="number" class="form-control" value="2.0" min="1">
+                                    <button class="btn btn-danger btn-sm">X</button>
+                                </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                Senior Application Developer <br> $315.00
+                                <div class="input-group input-group-sm" style="width: 100px;">
+                                    <input type="number" class="form-control" value="3.0" min="1">
+                                    <button class="btn btn-danger btn-sm">X</button>
+                                </div>
+                            </li>
+                        </ul>
 
 
+                    </div>
 
+                    <!-- total price align right -->
+                    <div class="my-4 text-end">
+                        <h5>Total: $752.50</h5>
+                    </div>
 
-            </form>
+                    <div class="form-group my-2 text-end">
+                        <button type="button" class="btn btn-outline-danger">Clear Order</button>
+                        <button type="button" class="btn btn-primary">Save Draft</button>
+                    </div>
+                </div>
+            </div>
         </div>
 
-    @endsection
+        <!-- modal that asks quantity and notes -->
+        <div class="modal fade" id="addCustomItemModal" tabindex="-1" aria-labelledby="addCustomItemModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addCustomItemModalLabel">Add Item
+                            <!-- badge for total price -->
+                            <span class="badge bg-primary">$137.50</span>
+                        </h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body my-2">
+                        <div class="form-group my-2">
+                            <label for="customItemName">Item Name:</label>
+                            <input value="Student Web Development ($25.00 per hour)" disabled type="text" name="customItemName" id="customItemName" class="form-control">
+                        </div>
+
+                        <div class="form-group my-2">
+                            <label for="customItemPrice">Unit Price:</label>
+                            <input type="number" name="customItemPrice" id="customItemPrice" value="25.00" disabled class="form-control">
+                        </div>
+
+                        <div class="form-group my-2">
+                            <label for="customItemQuantity">Quantity:</label>
+                            <input type="number" value="5.5" name="customItemQuantity" id="customItemQuantity" class="form-control">
+                        </div>
+
+                        <div class="form-group my-2">
+                            <label for="customItemNotes">Notes:</label>
+                            <textarea name="customItemNotes" id="customItemNotes" class="form-control">Student web development (Marisa Morneau) for 5.5 hours.</textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Add Item</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+@endsection
